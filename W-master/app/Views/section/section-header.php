e5df56040ba364d8de69d8d705c0fb52c728d931
webforce13 <webforce13@gmail.com>
2017-04-10 17:05:47 +0200

    formulaire de contact
    modification formulaire de contact

--- W-master/app/Views/section/section-header.php
+++ W-master/app/Views/section/section-header.php
@@ -40,7 +40,7 @@
   <div></div>   
   <div class="aerotop"> 
   <h1 class="text">AERO RECYCLING SOCIETY</h1>
-  <div class="logotop"><h3> - Société de recyclage aéronautique - <img src="./img/Aircraft-logo.png" alt="logoreact" id="logoreact"></h3></div>
+  <div class="logotop"><h3> - Société de recyclage aéronautique - <img src="../public/assets/img/Aircraft-logo.png" alt="logoreact" id="logoreact"></h3></div>
   </div>
   </header>
   <main>
