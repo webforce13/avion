2f83b602e7138ce292b4c8e4322be61809a12209
houss13 <mabrouk.houssam@hotmail.fr>
2017-04-10 14:48:01 +0200

    page contact
    je rajoute la page contact formulaire d'inscription

--- W-master/app/Views/page/contact.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* la page de contact pour le formulaire */ 
-$this->insert("section/section-header");
-$this->insert("section/section-contact");
-$this->insert("section/section-footer");
-?>
