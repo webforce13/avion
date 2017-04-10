8da112009b9024c20a0b267cc96b358b2df41cab
webforce13 <webforce13@gmail.com>
2017-04-10 14:46:33 +0200

    rajout header et footer

---
+++ W-master/app/Views/section/section-footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <ul>
+      <li><a href="" >A Propos De Nous</a></li>
+    </ul>  
+
+    <ul>
+      <img src="./img/logoA.png" alt="logoplane" id="logoplane">
+      <br>
+      <section class="footer-section">
+      <span>Contactez Nous :</span>
+      <br>
+      <span><a href="" title="Email"><i class="fa fa-envelope-o">  -  AeroRecycling@hotmail.com</i></a></span>
+      <br>
+      <span><a href="" title="Telephone"><i class="fa fa-phone">  -  06.12.34.56.78</i></a></span>
+      </section>
+      </ul>
+
+      <span class="copyright">©2017 “AERO RECYCLING SOCIETY - Marignane, France </span>
+      <span > - All Rights Reserved - </span>
+      </div>
+      </section>
+</footer>
---
+++ W-master/app/Views/section/section-header.php
@@ -0,0 +1,10 @@
+<header class="banner">
+	<div class="diamond">
+    <div></div>
+  	</div>
+  	<div></div>   
+  	<div class="aerotop"> 
+  		<h1 class="text">AERO RECYCLING SOCIETY</h1>
+  	<div class="logotop"><h3> - Société de recyclage aéronautique - <img src="./img/Aircraft-logo.png" alt="logoreact" id="logoreact"></h3></div>
+  	</div>
+</header>
