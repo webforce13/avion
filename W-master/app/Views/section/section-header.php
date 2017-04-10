c85a160166592e993c837896ced7395d6fc8ead1
webforce13 <webforce13@gmail.com>
2017-04-10 15:09:09 +0200

    modification header
    lancien etait incomplet

--- W-master/app/Views/section/section-header.php
+++ W-master/app/Views/section/section-header.php
@@ -1,10 +1,28 @@
-<header class="banner">
-	<div class="diamond">
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>HOMEPAGE</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Faster+One|Suez+One" rel="stylesheet">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+  <header class="banner">
+      <div class="diamond">
     <div></div>
-  	</div>
-  	<div></div>   
-  	<div class="aerotop"> 
-  		<h1 class="text">AERO RECYCLING SOCIETY</h1>
-  	<div class="logotop"><h3> - Société de recyclage aéronautique - <img src="./img/Aircraft-logo.png" alt="logoreact" id="logoreact"></h3></div>
-  	</div>
-</header>
+  </div>
+  <div></div>   
+  <div class="aerotop"> 
+  <h1 class="text">AERO RECYCLING SOCIETY</h1>
+  <div class="logotop"><h3> - Société de recyclage aéronautique - <img src="./img/Aircraft-logo.png" alt="logoreact" id="logoreact"></h3></div>
+  </div>
+  </header>
