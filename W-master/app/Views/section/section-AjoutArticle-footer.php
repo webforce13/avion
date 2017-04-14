0dd5af6ed66bf4cabff1e4c603e5968955e7d563
webforce13 <webforce13@gmail.com>
2017-04-14 10:54:14 +0200

    modif ajour article

--- W-master/app/Views/section/section-AjoutArticle-footer.php
+++ W-master/app/Views/section/section-AjoutArticle-footer.php
@@ -1,45 +1,30 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <title>HOMEPAGE</title>
+</main> 
+  <footer>
+      <ul>
+        <li><a href="" >A Propos De Nous</a></li>
+      </ul>  
 
-  <meta name="viewport" content="width=device-width">
-  <link href="https://fonts.googleapis.com/css?family=Faster+One|Suez+One" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+      <ul>
+        <img src="../public/assets/img/logoA.png" alt="logoplane" id="logoplane">
+        <br>
+        <section class="footer-section">
+        <span>Contactez Nous :</span>
+        <br>
+        <span><a href="" title="Email"><i class="fa fa-envelope-o">  -  Formulaire de Contact</i></a></span>
+        <br>
+        <span><a href="" title="Telephone"><i class="fa fa-phone">  -  06.12.34.56.78</i></a></span>
+        </section>
+        </ul>
 
+        <span class="copyright">©2017 “DEEE RECYCLAGE AERONAUTIQUE - Marignane, France </span>
+        <span > - All Rights Reserved - </span>
+        </div>
+        </section>
+  </footer>
   
-  <!-- Boostrap-->
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <script src="../public/assets/js/main.js"></script>
+  <script src="../public/assets/js/index.js"></script>
 
-  <link href="../public/assets/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  </body>
+  </html>
 
-
-
-  <!-- lien pour le CSS -->
-  <link rel="stylesheet" href="../public/assets/css/style.css">
-
-  <!-- lien pour le jquery -->
-  <script
-      src="https://code.jquery.com/jquery-3.1.1.min.js"
-      integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-      crossorigin="anonymous">
-  </script>
-
-
-
-  
-</head>
-
-<body>
-  <header class="banner">
-      <div class="diamond">
-    <div></div>
-  </div>
-  <div></div>   
-  <div class="aerotop"> 
-  <h1 class="text">AERO RECYCLING SOCIETY</h1>
-  <div class="logotop"><h3> - Société de recyclage aéronautique - <img src="../public/assets/img/Aircraft-logo.png" alt="logoreact" id="logoreact"></h3></div>
-  </div>
-  </header>
-  <main>
--- W-master/app/Views/section/section-AjoutArticle.php
+++ W-master/app/Views/section/section-AjoutArticle.php
@@ -1,28 +1,53 @@
-<form action="" method="POST" enctype="multipart/form-data">
-	<label for="ref">Référence</label>
-	<input id="ref" name="ref" placeholder="mettre la Référence" type="text">
+<form action="" class="form-horizontal"  method="POST" enctype="multipart/form-data">
+
+
+<div class="form-group">
+	<label for="ref" class="col-sm-2 control-label">Référence</label>
+	<input id="ref" name="ref" placeholder="Entrer la Référence de votre article" type="text">
+</div>	
+
 	<br>
-	<label for="designation">Designation</label>
-	<input id="designation" name="designation" placeholder="Designation" type="text">
+
+<div class="form-group">	
+	<label for="designation" class="col-sm-2 control-label">Désignation</label>
+	<input id="designation" name="designation" placeholder="Désignation de votre article" type="text">
+</div>
+
 	<br>
-	<label for="condition">Etat de la piece</label>
+
+<div class="form-group">	
+	<label for="condition" class="col-sm-2 control-label">Etat de la piece</label>
 	<select name="condition" value="">
 		<option value="Bon" selected id="Bon">BON</option>
 		<option value="Moyen" id="Moyen">Moyen</option>
 		<option value="Mauvais" id="Mauvais">Mauvais</option>
 	</select>
-	<br>
-	<label for="Description">La Description de la piece</label>
-	<textarea name="description" id="Description" cols="30" rows="10"></textarea>		
-	<br>
-	<label for="quantite">Les Quantitées des pieces Disponible </label>
-	<input type="number" min="0" name="quantite" id="quantite">
-	<br>
-	<label for="image">Inserée photo 1:</label>
-	<input name="image" id="image" type="file">
-	<br>	
-	<button type="submit" name="btnSub" >ENVOYER</button>
+</div>
 
+	<br>
+
+<div class="form-group">
+	<label for="Description" class="col-sm-2 control-label">La Description de la piece</label>
+	<textarea name="description" id="Description" cols="30" rows="10"></textarea>	
+</div>
+
+	<br>
+
+<div class="form-group">
+	<label for="quantite" class="col-sm-2 control-label">Les Quantitées des pieces Disponible </label>
+	<input type="number" min="0" name="quantite" id="quantite">
+</div>
+
+	<br>
+
+<div class="form-group">	
+	<label for="image" class="col-sm-2 control-label">Inserée photo 1:</label>
+	<input name="image" id="image" type="file">
+</div>
+
+	<br>	
+
+	<button type="submit" name="btn btn-default" >ENVOYER</button>
 	<input type="hidden" name="operation" value="ajouter">
 	<div>
 		<?php if (isset($message)) echo $message; ?>
