548690a8dbca10a63dbe5d8e528d0ca91c60ad93
thibb84 <thibb84@gmail.com>
2017-04-19 14:26:18 +0200

    changement
    page info

--- W-master/app/Views/section/section-header-accueil.php
+++ W-master/app/Views/section/section-header-accueil.php
@@ -9,7 +9,7 @@
 
 	
 	<!-- Boostrap-->
-	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<link href="../public/assets/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
