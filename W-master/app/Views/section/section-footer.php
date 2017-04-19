8f8cab0025f4f010147ff9cfc284aa25381e851c
webforce13 <webforce13@gmail.com>
2017-04-19 11:09:41 +0200

    footer par defaut

--- W-master/app/Views/section/section-footer.php
+++ W-master/app/Views/section/section-footer.php
@@ -7,17 +7,20 @@
       <ul>
         <img src="../public/assets/img/logoA.png" alt="logoplane" id="logoplane">
         <br>
+        <br>
         <section class="footer-section">
         <span>Contactez Nous :</span>
         <br>
+        <br>
         <span><a href="contact" title="Email"><i class="fa fa-envelope-o">  -  Formulaire de Contact</i></a></span>
+        <br>
         <br>
         <span><a href="" title="Telephone"><i class="fa fa-phone">  -  06.12.34.56.78</i></a></span>
         </section>
         </ul>
-
-        <span class="copyright">©2017 “DEEE RECYCLAGE AERONAUTIQUE - Marignane, France </span>
-        <span > - All Rights Reserved - </span>
+<br>
+<br>
+        <span class="copyright">©2017 “DEEE RECYCLAGE AERONAUTIQUE - Marignane, France - All Rights Reserved - </span>
         </div>
         </section>
   </footer>
